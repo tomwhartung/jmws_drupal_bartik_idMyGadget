74658f4e7f4b6e8b231590d514ce2dd7783879cd
Tom W Hartung <tomwhartung@gmail.com>
2015-10-14 17:13:40 -0600

    Added the jmwsIdMyGadget object to templates/page.tpl.php , we now have the nice styling for the main menu back.

--- sites/all/themes/jmws/bartik_idMyGadget/templates/page.tpl.php
+++ sites/all/themes/jmws/bartik_idMyGadget/templates/page.tpl.php
@@ -84,41 +84,46 @@
  * @see bartik_process_page()
  * @see html.tpl.php
  */
+global $jmwsIdMyGadget;
 ?>
 <div id="page-wrapper"><div id="page">
 
   <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
+    <?php if ( isset($jmwsIdMyGadget) && $jmwsIdMyGadget->isEnabled() ) : ?>
+      <?php echo $jmwsIdMyGadget->getLogoNameTitleDescriptionHtml($front_page) ?>
+    <?php else : ?>
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
 
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
+                <strong>
+                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                </strong>
+              </div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
                 <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
+              </h1>
+            <?php endif; ?>
           <?php endif; ?>
-        <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+              <?php print $site_slogan; ?>
+            </div>
+          <?php endif; ?>
 
-      </div> <!-- /#name-and-slogan -->
+        </div> <!-- /#name-and-slogan -->
+      <?php endif; ?>
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
